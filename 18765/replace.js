72c44977acbb7a1e6fd440c4f5a41930e4e40697
iamgamja <hmmthinking@naver.com>
2022-06-16 11:43:48 +0900

    Create replace.js

---
+++ 18765/replace.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+const solve = require('./solve.js')
+
+const txt = fs
+	.readFileSync('output.txt')
+	.toString()
+	.split('\n')
+
+for (let i of txt) {
+	const target = i.replace(
+    '[+[]]-[]+!![]',
+    '[+!![]]-[]'
+  )
+
+	solve(txt, target, true, 0)
+	solve(txt, target + '+!![]', true, 0)
+	solve(txt, target + '-!![]', true, 0)
+	solve(txt, target + '+[]', true, 0)
+	solve(txt, target + '-[]', true, 0)
+
+}
+
+fs.writeFileSync('output.txt', txt.join('\n'))
+
+console.log('📏', txt.join('\n').length)
