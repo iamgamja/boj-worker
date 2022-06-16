2becf58e0e5be7ae163251f9f49d590d785c1531
iamgamja <hmmthinking@naver.com>
2022-06-16 12:11:26 +0900

    Update replace.js

--- 18765/replace.js
+++ 18765/replace.js
@@ -3,21 +3,21 @@ const fs = require('fs')
 const solve = require('./solve.js')
 
 const txt = fs
-	.readFileSync('output.txt')
-	.toString()
-	.split('\n')
+  .readFileSync('output.txt')
+  .toString()
+  .split('\n')
 
 for (let i of txt) {
-	const target = i.replace(
+  const target = i.replace(
     '[+[]]-[]+!![]',
     '[+!![]]-[]'
   )
 
-	solve(txt, target, true, 0)
-	solve(txt, target + '+!![]', true, 0)
-	solve(txt, target + '-!![]', true, 0)
-	solve(txt, target + '+[]', true, 0)
-	solve(txt, target + '-[]', true, 0)
+  solve(txt, target, true, 0)
+  solve(txt, target + '+!![]', true, 0)
+  solve(txt, target + '-!![]', true, 0)
+  solve(txt, target + '+[]', true, 0)
+  solve(txt, target + '-[]', true, 0)
 
 }
 
